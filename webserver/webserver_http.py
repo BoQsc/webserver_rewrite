ced9aeafe470aa490df13f150687e7e829449a56
Feldwor <BoQsc@users.noreply.github.com>
2025-11-03 00:02:16 +0200

    up

---
+++ webserver/webserver_http.py
@@ -0,0 +1,17 @@
+
+import http.server
+import socketserver
+
+PORT = 8000
+
+class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(b"Hello, World!")
+
+def run_server():
+    with socketserver.TCPServer(("", PORT), MyHttpRequestHandler) as httpd:
+        print("serving at port", PORT)
+        httpd.serve_forever()
